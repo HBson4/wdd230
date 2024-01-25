ee78905df90ef6ab79ff3fde00de9d68da345367
HBson4 <101207606+HBson4@users.noreply.github.com>
2024-01-25 09:15:03 -0700

    completed activity

---
+++ week02/scripts/bom.js
@@ -0,0 +1,28 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+
+button.addEventListener('click', () => {
+    const value =  input.value;
+    if (value) {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li)
+            input.focus();
+        })
+
+        li.appendChild(deleteButton);
+        list.appendChild(li)
+
+        input.value = '';
+        input.focus();
+    }
+    
+    input.focus();
+});
