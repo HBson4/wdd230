65d29977f677eb6fb4237e35ce947233b1202890
HBson4 <101207606+HBson4@users.noreply.github.com>
2024-01-30 11:05:06 -0700

    implemented dark mode

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,16 @@
+// Hamburger Menu
 const hamburgerElement = document.querySelector('#navButton');
 const navElement = document.querySelector('#navLinks');
 
 hamburgerElement.addEventListener('click', () => {
     navElement.classList.toggle('open');
     hamburgerElement.classList.toggle('open');
+});
+
+// Dark Mode
+const darkButton = document.querySelector('#darkBtn');
+const main = document.querySelector('main')
+
+darkButton.addEventListener('click', () => {
+    main.classList.toggle('dark');
 });
