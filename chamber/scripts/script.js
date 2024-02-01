15f6876a1da1fbd5b5b4122e7b17407375bb1961
HBson4 <101207606+HBson4@users.noreply.github.com>
2024-02-01 10:15:26 -0700

    added some css

---
+++ chamber/scripts/script.js
@@ -0,0 +1,8 @@
+// Hamburger Menu
+const hamburgerElement = document.querySelector('#navButton');
+const navElement = document.querySelector('#navLinks');
+
+hamburgerElement.addEventListener('click', () => {
+    navElement.classList.toggle('open');
+    hamburgerElement.classList.toggle('open');
+});
