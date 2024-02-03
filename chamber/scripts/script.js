06a9fcd9ee96a5aabd7e3236632f637be99a2bb1
HBson4 <101207606+HBson4@users.noreply.github.com>
2024-02-02 17:20:31 -0700

    footer added

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -6,3 +6,14 @@ hamburgerElement.addEventListener('click', () => {
     navElement.classList.toggle('open');
     hamburgerElement.classList.toggle('open');
 });
+
+// Get last modified date
+const lastModifiedElement = document.querySelector('#lastModified');
+const lastModifiedDate = document.lastModified;
+lastModifiedElement.innerHTML = `Last Modified: ${lastModifiedDate}`;
+
+
+// Get current year
+const currentYearElement = document.querySelector('#year');
+const currentYear = new Date().getFullYear();
+currentYearElement.innerHTML = `&copy;${currentYear}`;
