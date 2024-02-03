0aa161c586e7e62d57998bd7f6b6076a321e61c3
HBson4 <101207606+HBson4@users.noreply.github.com>
2024-02-02 19:48:42 -0700

    added image slideshow

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -17,3 +17,27 @@ lastModifiedElement.innerHTML = `Last Modified: ${lastModifiedDate}`;
 const currentYearElement = document.querySelector('#year');
 const currentYear = new Date().getFullYear();
 currentYearElement.innerHTML = `&copy;${currentYear}`;
+
+// IMAGE SLIDESHOW
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
