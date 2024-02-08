945ce8f769ad4f2883d5e151524169430e0d9429
HBson4 <101207606+HBson4@users.noreply.github.com>
2024-02-08 09:22:22 -0700

    removed old bom js

--- week03/scripts/bom.js
+++ week03/scripts/bom.js
@@ -1,34 +1,3 @@
-// const input = document.querySelector('input');
-// const button = document.querySelector('button');
-// const list = document.querySelector('#list');
-
-
-// button.addEventListener('click', (e) => {
-//     e.preventDefault();
-    
-//     const value =  input.value;
-//     if (value) {
-//         const li = document.createElement('li');
-//         const deleteButton = document.createElement('button');
-
-//         li.textContent = input.value;
-//         deleteButton.textContent = '❌';
-
-//         deleteButton.addEventListener('click', () => {
-//             list.removeChild(li)
-//             input.focus();
-//         })
-
-//         li.appendChild(deleteButton);
-//         list.appendChild(li)
-
-//         input.value = '';
-//         input.focus();
-//     }
-
-//     input.focus();
-// });
-
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
