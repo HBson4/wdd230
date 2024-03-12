48515938ac05756e3a284c80091ae0082652f05a
HBson4 <101207606+HBson4@users.noreply.github.com>
2024-03-12 13:45:44 -0600

    links styled

--- scripts/links.js
+++ scripts/links.js
@@ -1,36 +1,13 @@
-const baseURL = "http://127.0.0.1:5500/index.html";
-const linksURL = "http://127.0.0.1:5500/data/links.json";
-
-// const baseURL = "https://hbson4.github.io/wdd230/";
-// const linksURL = "https://hbson4.github.io/wdd230/data/links.json";
+const baseURL = "https://hbson4.github.io/wdd230/";
+const linksURL = "https://hbson4.github.io/wdd230/data/links.json";
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    // console.log(data);
     displayLinks(data);
 }
   
 getLinks();
-
-// function displayLinks(data) {
-//     const ul = document.querySelector("#weeks");
-
-//     data.weeks.forEach(week => {
-//         const li = document.createElement("li");
-//         li.innerHTML = `${week.week}: `;
-
-//         data.forEach(link => {
-//             const a = document.createElement("a");
-
-//             a.setAttribute("href", link.url);
-//             a.textContent = link.title;
-
-//             li.appendChild(a);
-//         });
-//         ul.appendChild(li);
-//     });
-// }
 
 function displayLinks(weeks) {
     const weeksUl = document.getElementById("weeks");
@@ -44,10 +21,12 @@ function displayLinks(weeks) {
 
         week.links.forEach(link => {
             const a = document.createElement("a");
+            const bar = document.createTextNode("|");
 
             a.setAttribute("href", link.url);
             a.textContent = link.title;
 
+            li.appendChild(bar);
             li.appendChild(a);
         });
         weeksUl.appendChild(li);
--- scripts/weather.js
+++ scripts/weather.js
@@ -1,7 +1,7 @@
 const temp = document.querySelector('#temp')
 const icon = document.querySelector('#icon');
 
-// const url = 'https://api.openweathermap.org/data/2.5/weather?lat=33.41511383579722&lon=-111.82878674211621&appid=a91aee95660dbba1541c15246bd7ab75&units=imperial';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=33.41511383579722&lon=-111.82878674211621&appid=a91aee95660dbba1541c15246bd7ab75&units=imperial';
 
 async function apiFetch() {
     try {
