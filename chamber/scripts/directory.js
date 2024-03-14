bd13c36f3b875e833230ef5eb5f235d65cd2f226
HBson4 <101207606+HBson4@users.noreply.github.com>
2024-03-14 09:38:26 -0600

    directory info showing

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -6,6 +6,42 @@ async function fetchDirectoryData() {
   displayDirectory(data);
 }
 
+fetchDirectoryData();
+
 function displayDirectory(data) {
-  console.log(data);
+  const directory = document.querySelector('main');
+
+  for (const member of data) {
+    const section = document.createElement('section');
+
+    const image = document.createElement('img');
+    image.setAttribute('src', member.image);
+    image.setAttribute('alt', `${member.name} logo`);
+
+    const name = document.createElement('p');
+    name.textContent = member.name;
+
+    const address = document.createElement('p');
+    address.textContent = member.address;
+
+    const phoneNumber = document.createElement('p');
+    phoneNumber.textContent = member.phoneNumber;
+
+    const websiteUrl = document.createElement('a');
+    websiteUrl.setAttribute('href', member.websiteUrl);
+    websiteUrl.textContent = 'Website';
+
+    const membershipLvl = document.createElement('p');
+    membershipLvl.textContent = member.membershipLvl;
+
+    section.appendChild(image);
+    section.appendChild(name);
+    section.appendChild(address);
+    section.appendChild(phoneNumber);
+    section.appendChild(websiteUrl);
+    section.appendChild(membershipLvl);
+
+    directory.appendChild(section);
+    };
 };
+
