6c4f4b037bcf8daf5e1d8bfd5d0374d499367ce5
HBson4 <101207606+HBson4@users.noreply.github.com>
2024-03-21 09:40:33 -0600

    company spotlight js

--- chamber/scripts/slideshow.js
+++ chamber/scripts/slideshow.js
@@ -1,3 +1,61 @@
+const url = './data/members.json';
+let companies = [];
+const img1 = document.querySelector('.logo1');
+const img2 = document.querySelector('.logo2');
+const img3 = document.querySelector('.logo3');
+
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  getCompanies(data);
+}
+
+function getCompanies(data) {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].membershipLvl === 'silver' || data[i].membershipLvl === 'gold') {
+      companies.push(data[i]);
+    }
+  }
+  companies = shuffle(companies);
+  displayData(companies);
+}
+
+// Randomize array
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+  
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+  return array;
+}
+
+// Display first 3 company logos
+function displayData(companies) {
+
+  for (let i = 0; i < 3 && i < companies.length; i++) {
+    if (i == 0) {
+      img1.setAttribute('src', companies[i].image.hyperlink);
+    }
+    else if (i == 1) {
+      img2.setAttribute('src', companies[i].image.hyperlink);
+    }
+    else if (i == 2) {
+      img3.setAttribute('src', companies[i].image.hyperlink);
+    }
+  }
+}
+
+fetchData();
+
+
 // IMAGE SLIDESHOW
 let slideIndex = 0;
 showSlides();
