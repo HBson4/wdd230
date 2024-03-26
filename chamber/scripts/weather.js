5280c7d8325e8a265078a7aa0114e552275278a9
HBson4 <101207606+HBson4@users.noreply.github.com>
2024-03-26 09:32:56 -0600

    started weather.js for home

---
+++ chamber/scripts/weather.js
@@ -0,0 +1,19 @@
+const url = "api.openweathermap.org/data/2.5/forecast?lat=33.44699029384253&lon=-112.07018528539054&appid=a91aee95660dbba1541c15246bd7ab75&units=imperial";
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            // displayResults(data);
+        } 
+        else {
+            throw Error(await response.text());
+        }}
+        catch (error) {
+            console.log(error);
+    }
+}
+
+apiFetch();
