8b543d7505ed5f17f29ed3d2c1cff7ac9a42bf32
HBson4 <101207606+HBson4@users.noreply.github.com>
2024-03-26 09:22:03 -0600

    added banner to home page

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -18,4 +18,24 @@ const currentYearElement = document.querySelector('#year');
 const currentYear = new Date().getFullYear();
 currentYearElement.innerHTML = `&copy;${currentYear}`;
 
-// Date user last visited
+// Date user last visited (for discover page)
+
+
+// Home page banner
+function closeBanner() {
+    document.getElementById("banner").style.display = "none";
+}
+
+function isBannerDay() {
+    const today = new Date();
+    const dayOfWeek = today.getDay();
+    return dayOfWeek >= 1 && dayOfWeek <= 3;
+}
+
+function showBannerOnSpecificDays() {
+    if (isBannerDay()) {
+        document.getElementById("banner").style.display = "block";
+    }
+}
+
+window.onload = showBannerOnSpecificDays();
