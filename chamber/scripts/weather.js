466ffa582b02b90622eac54d26cc54d5c3a2b977
HBson4 <101207606+HBson4@users.noreply.github.com>
2024-03-29 10:04:01 -0700

    fixed api call

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,8 +1,8 @@
-const url = "api.openweathermap.org/data/2.5/forecast?lat=33.44699029384253&lon=-112.07018528539054&appid=a91aee95660dbba1541c15246bd7ab75&units=imperial";
+const api = 'https://api.openweathermap.org/data/2.5/forecast?lat=33.44699029384253&lon=-112.07018528539054&appid=a91aee95660dbba1541c15246bd7ab75&units=imperial';
 
 async function apiFetch() {
     try {
-        const response = await fetch(url);
+        const response = await fetch(api);
         if (response.ok) {
             const data = await response.json();
             console.log(data);
