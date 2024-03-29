eab7b37edc8a4f284ef02cf7cf0e3c38d2030eee
HBson4 <101207606+HBson4@users.noreply.github.com>
2024-03-29 12:53:44 -0700

    added day names for weather forcast

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -25,7 +25,26 @@ apiFetch();
 
 function displayResults(data) {
     current_temp.innerHTML = `Current Temp:${data.list[0].main.temp.toFixed(0)}&deg; F - ${data.list[0].weather[0].description}`;
-    day1.innerHTML = `Day 1: ${data.list[8].main.temp.toFixed(0)}&deg; F - ${data.list[8].weather[0].description}`;
-    day2.innerHTML = `Day 2: ${data.list[16].main.temp.toFixed(0)}&deg; F - ${data.list[16].weather[0].description}`;
-    day3.innerHTML = `Day 3: ${data.list[24].main.temp.toFixed(0)}&deg; F - ${data.list[24].weather[0].description}`;
+    day1.innerHTML = `${getDay(1)}: ${data.list[8].main.temp.toFixed(0)}&deg; F - ${data.list[8].weather[0].description}`;
+    day2.innerHTML = `${getDay(2)}: ${data.list[16].main.temp.toFixed(0)}&deg; F - ${data.list[16].weather[0].description}`;
+    day3.innerHTML = `${getDay(3)}: ${data.list[24].main.temp.toFixed(0)}&deg; F - ${data.list[24].weather[0].description}`;
+}
+
+function getDay(num) {
+    const date = new Date();
+    const weekday = new Array(7);
+
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    if (date.getDay() + num > 6) {
+        return weekday[date.getDay() + num - 7];
+    }
+
+    return weekday[date.getDay() + num];
 }
