b9168eb0308364d9cde6c6dacc2e8b7e8ded8c5d
HBson4 <101207606+HBson4@users.noreply.github.com>
2024-03-29 20:04:51 -0700

    styled weather

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,7 +1,10 @@
-const current_temp = document.querySelector('#current_temp');
+const current_desc = document.querySelector('#current_desc');
 const day1 = document.querySelector('#day1');
 const day2 = document.querySelector('#day2');
 const day3 = document.querySelector('#day3');
+const desc1 = document.querySelector('#desc1');
+const desc2 = document.querySelector('#desc2');
+const desc3 = document.querySelector('#desc3');
 
 const api = 'https://api.openweathermap.org/data/2.5/forecast?lat=33.44699029384253&lon=-112.07018528539054&appid=a91aee95660dbba1541c15246bd7ab75&units=imperial';
 
@@ -24,10 +27,16 @@ async function apiFetch() {
 apiFetch();
 
 function displayResults(data) {
-    current_temp.innerHTML = `Current Temp:${data.list[0].main.temp.toFixed(0)}&deg; F - ${data.list[0].weather[0].description}`;
-    day1.innerHTML = `${getDay(1)}: ${data.list[8].main.temp.toFixed(0)}&deg; F - ${data.list[8].weather[0].description}`;
-    day2.innerHTML = `${getDay(2)}: ${data.list[16].main.temp.toFixed(0)}&deg; F - ${data.list[16].weather[0].description}`;
-    day3.innerHTML = `${getDay(3)}: ${data.list[24].main.temp.toFixed(0)}&deg; F - ${data.list[24].weather[0].description}`;
+    current_desc.innerHTML = `${data.list[0].main.temp.toFixed(0)}&deg; F - ${data.list[0].weather[0].description}`;
+
+    day1.innerHTML = `${getDay(1)}`;
+    desc1.innerHTML = `${data.list[8].main.temp.toFixed(0)}&deg; F - ${data.list[8].weather[0].description}`;
+
+    day2.innerHTML = `${getDay(2)}`;
+    desc2.innerHTML = `${data.list[16].main.temp.toFixed(0)}&deg; F - ${data.list[16].weather[0].description}`;
+
+    day3.innerHTML = `${getDay(3)}`;
+    desc3.innerHTML = `${data.list[24].main.temp.toFixed(0)}&deg; F - ${data.list[24].weather[0].description}`;
 }
 
 function getDay(num) {
