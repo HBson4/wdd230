a0d5e5446540df2f5f38015b972270d14e7533cd
HBson4 <101207606+HBson4@users.noreply.github.com>
2024-04-04 08:48:15 -0600

    fixed directory layout

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -6,64 +6,48 @@ async function fetchDirectoryData() {
   displayDirectory(data);
 }
 
-fetchDirectoryData();
-
 function displayDirectory(data) {
   const directory = document.querySelector('#directory-info');
-  directory.classList.add('gridCards');
+  directory.classList.add('grid');
 
-  for (const member of data) {
-    const section = document.createElement('section');
-    section.classList.add('card');
-
-    const image = document.createElement('img');
-    image.setAttribute('src', member.image.hyperlink);
-    image.setAttribute('alt', `${member.name} logo`);
-
-    const name = document.createElement('h4');
-    name.textContent = member.name;
-
-    const address = document.createElement('p');
-    address.textContent = member.address;
-
-    const phoneNumber = document.createElement('p');
-    phoneNumber.textContent = member.phoneNumber;
-
-    const websiteUrl = document.createElement('a');
-    websiteUrl.setAttribute('href', member.websiteUrl);
-    websiteUrl.textContent = 'Website';
-
-    const membershipLvl = document.createElement('p');
-    membershipLvl.textContent = member.membershipLvl.toUpperCase();
-
-    section.appendChild(image);
-    section.appendChild(name);
-    section.appendChild(address);
-    section.appendChild(phoneNumber);
-    section.appendChild(websiteUrl);
-    section.appendChild(membershipLvl);
-
-    directory.appendChild(section);
-  };
+  data.forEach((member) => {
+    const card = `
+      <section class="card">
+        <img src="${member.image.hyperlink}" alt="${member.name} logo">
+        <h4>${member.name}</h4>
+        <p>${member.address}</p>
+        <p>${member.phoneNumber}</p>
+        <a href="${member.websiteUrl}">Website</a>
+        <p>${member.membershipLvl.toUpperCase()}</p>
+      </section>
+      `;
+      directory.insertAdjacentHTML('beforeend', card);
+  });
 };
+
+fetchDirectoryData();
 
 const cardBtn = document.querySelector('#cardBtn');
 const listBtn = document.querySelector('#listBtn');
 
 cardBtn.addEventListener('click', () => {
   const sections = document.querySelectorAll('.list');
+  const directory = document.querySelector('#directory-info');
 
   sections.forEach(section => {
     section.classList.add('card');
+    directory.classList.add('grid');
     section.classList.remove('list');
   });
 });
 
 listBtn.addEventListener('click', () => {
   const sections = document.querySelectorAll('.card');
+  const directory = document.querySelector('#directory-info');
 
   sections.forEach(section => {
     section.classList.add('list');
+    directory.classList.remove('grid');
     section.classList.remove('card');
   });
 });
